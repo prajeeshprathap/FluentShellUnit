84912ec988ca3299b3586bcdd05ab8240fbe9ed4
prajeeshprathap <prajeesh.prathap@gmail.com>
2015-02-20 23:30:30 +0000

    Added PsCodeMethod and PsObjectExtensions

---
+++ FluentShellUnit/PSObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentShellUnit
+{
+    public static class PSObjectExtensions
+    {
+        public static PSObject StubMethod(this PSObject psObject, string name, Func<object> method)
+        {
+            psObject.Methods.Add(new PsCodeMethod(name, method));
+            return psObject;
+        }
+        public static PSObject StubMethod(this PSObject psObject, string name, PsDelegate psDelegate)
+        {
+            psObject.Methods.Add(new PsCodeMethod(name, psDelegate));
+            return psObject;
+        }
+
+        public static PSObject StubProperty(this PSObject psObject, string name, object value)
+        {
+            psObject.Properties.Add(new PSNoteProperty(name, value));
+            return psObject;
+        }
+    }
+}
---
+++ FluentShellUnit/PsCodeMethod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace FluentShellUnit
+{
+    public delegate object PsDelegate(params object[] argsObjects);
+    public class PsCodeMethod : PSMethodInfo
+    {
+        private readonly string _name;
+        private readonly PsDelegate _action;
+        private readonly Func<object> _method;
+
+        public PsCodeMethod(string name, Func<object> method)
+        {
+            _name = name;
+            _method = method;
+
+            var nameField = this.GetType().GetField("name",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            nameField?.SetValue(this, name);
+        }
+
+        public PsCodeMethod(string name, PsDelegate action)
+        {
+            _name = name;
+            _action = action;
+
+            var nameField = this.GetType().GetField("name",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            nameField?.SetValue(this, name);
+        }
+
+        public override PSMemberInfo Copy()
+        {
+            if (_method != null)
+            {
+                return new PsCodeMethod(_name, _method);
+            }
+            return new PsCodeMethod(_name, _action);
+        }
+
+        public override PSMemberTypes MemberType => PSMemberTypes.CodeMethod;
+
+        public override string TypeNameOfValue => typeof (PSObject).ToString();
+
+        public override object Invoke(params object[] arguments)
+        {
+            return _method != null ? _method.Invoke() : _action.Invoke(arguments);
+        }
+
+        public override Collection<string> OverloadDefinitions => new Collection<string>();
+    }
+}
